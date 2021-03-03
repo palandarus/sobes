2255bf35bb11d6139a86daa8ae32d8991f27afe8
palandarus <klarkken@yandex.ru>
2021-03-03 15:05:07 +0300

    HWLone добавлена реализация паттерна строитель

---
+++ src/main/java/HWLone/Person.java
@@ -0,0 +1,69 @@
+package HWLone;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String country;
+    private String phone;
+    private int age;
+    private String gender;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
