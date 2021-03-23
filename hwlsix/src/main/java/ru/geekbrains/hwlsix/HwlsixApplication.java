380dd105adfddbf86e1f862f3c7657a1e7f63b29
palandarus <klarkken@yandex.ru>
2021-03-23 17:16:12 +0300

    HWLsix Добавил простейший spring-boot проект, и простейший http get/post клиент

---
+++ hwlsix/src/main/java/ru/geekbrains/hwlsix/HwlsixApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hwlsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwlsixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwlsixApplication.class, args);
+    }
+
+}
